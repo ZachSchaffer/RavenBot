f98680d71703c9631e681ce1adf04f7e5eb8ddeb
Zach Schaffer <zachary.schaffer12@gmail.com>
2020-03-03 13:56:47 -0500

    Split up functions

--- index.js
+++ index.js
@@ -38,56 +38,65 @@ bot.on("message", async message => {
   const command = args.shift().toLowerCase();
 
   if (command === "ban") {
-    console.log(message.author + " tried to ban " + message.mentions.members.first().user.tag);
-    if (!message.member.roles.some(r => ["Administrator", "Admin"].includes(r.name))) {
-      return message.reply(
-        "You do not have permission to ban. This incident will be recorded."
-      );
-    }
-
-
-    let member = message.mentions.members.first();
-    if (!member)
-      return message.reply("Invalid member of server.");
-    if (!member.bannable)
-      return message.reply(
-        "You cannot ban a superior. This incident will be recorded."
-      );
-
-    let reason = args.slice(1).join(" ");
-    if (!reason) reason = "No reason provided.";
-
-    await member
-      .ban(reason)
-      .catch(error =>
-        message.reply(
-          `${message.author} I couldn't ban because: ${error}`
-        )
-      );
-    message.reply(
-      `${member.user.tag} has been banned by ${message.author.tag} because ${reason}`
-    );
+    return ban(args, message);
   }
 
   if (command === "purge") {
-
-    const deleteCount = parseInt(args[0], 10);
-
-    if (!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply(
-        "Please provide a number between 2 and 100 for the number of messages to delete"
-      );
-
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({
-      limit: deleteCount
-    });
-    message.channel
-      .bulkDelete(fetched)
-      .catch(error =>
-        message.reply(`Couldn't delete messages because of: ${error}`)
-      );
+    return purge(args, message);
   }
 });
+
+function ban(args, message) {
+  console.log(message.author + " tried to ban " + message.mentions.members.first().user.tag);
+  if (!message.member.roles.some(r => ["Administrator", "Admin"].includes(r.name))) {
+    return message.reply(
+      "You do not have permission to ban. This incident will be recorded."
+    );
+  }
+
+  let member = message.mentions.members.first();
+  if (!member)
+    return message.reply("Invalid member of server.");
+  if (!member.bannable)
+    return message.reply(
+      "You cannot ban a superior. This incident will be recorded."
+    );
+
+  let reason = args.slice(1).join(" ");
+  if (!reason) reason = "No reason provided.";
+
+  await member
+    .ban(reason)
+    .catch(error =>
+      message.reply(
+        `${message.author} I couldn't ban because: ${error}`
+      )
+    );
+  message.reply(
+    `${member.user.tag} has been banned by ${message.author.tag} because ${reason}`
+  );
+}
+
+function purge(args, message) {
+  const deleteCount = parseInt(args[0], 10);
+
+  if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+    return message.reply(
+      "Please provide a number between 2 and 100 for the number of messages to delete"
+    );
+
+  const fetched = await message.channel.fetchMessages({
+    limit: deleteCount
+  });
+  message.channel
+    .bulkDelete(fetched)
+    .catch(error =>
+      message.reply(`Couldn't delete messages because of: ${error}`)
+    );
+}
+
+function say(args, message) {
+  return message;
+}
 
 bot.login(config.token);
