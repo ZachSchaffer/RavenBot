2a2e23e7d6c26df0434eaeeaa70d095413388d65
Zach Schaffer <zachary.schaffer12@gmail.com>
2020-03-03 13:27:29 -0500

    update packages and index.js

--- index.js
+++ index.js
@@ -1,89 +1,95 @@
-// Load up the discord.js library
 const Discord = require("discord.js");
-
-// This is your bot. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `bot.something`, or `bot.something`,
-// this is what we're refering to. Your bot.
 const bot = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./botconfig.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
 
 bot.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `bot.user` is what the
-  // docs refer to as the "ClientUser".
-  bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
+  console.log(
+    `Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`
+  );
+  bot.user.setActivity("Keeping an eye on things");
 });
 
 bot.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  console.log(
+    `New server joined: ${guild.name} (id: ${guild.id}). This server has ${guild.memberCount} members!`
+  );
   bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
 
 bot.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
   bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
 
-
 bot.on("message", async message => {
-  // This event will run on every single message received, from any channel or DM.
-  
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
-  if(message.author.bot) return;
-  
-  // Also good practice to ignore any message that does not start with our prefix, 
-  // which is set in the configuration file.
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  // Here we separate our "command" name, and our "arguments" for the command. 
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-  if(command === "ban") {
-    // Most of this command is identical to kick, except that here we'll only let admins do it.
-    // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.bannable) 
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+  // Ignore other bots
+  if (message.author.bot) return;
 
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  // Also good practice to ignore any message that does not start with our prefix,
+  // which is set in the configuration file.
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  // split command e.g. args = ["Is", "this", "the", "real", "life"]
+  const args = message.content
+    .slice(config.prefix.length)
+    .trim()
+    .split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === "ban") {
+    if (!message.member.roles.some(r => ["Administrator", "Admin"].includes(r.name))) {
+      console.log(message.author + " tried to ban " + message.mentions.members.first());
+      return message.reply(
+        "You do not have permission to ban. This incident will be recorded."
+      );
+    }
+
+
+    let member = message.mentions.members.first();
+    if (!member)
+      return message.reply("Invalid member of server.");
+    if (!member.bannable)
+      return message.reply(
+        "You cannot ban a superior. This incident will be recorded."
+      );
+
+    let reason = args.slice(1).join(" ");
+    if (!reason) reason = "No reason provided";
+
+    await member
+      .ban(reason)
+      .catch(error =>
+        message.reply(
+          `Sorry ${message.author} I couldn't ban because of : ${error}`
+        )
+      );
+    message.reply(
+      `${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`
+    );
   }
-  
-  if(command === "purge") {
+
+  if (command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
-    
+
     // get the delete count, as an actual number.
     const deleteCount = parseInt(args[0], 10);
-    
+
     // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
+    if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply(
+        "Please provide a number between 2 and 100 for the number of messages to delete"
+      );
+
     // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    const fetched = await message.channel.fetchMessages({
+      limit: deleteCount
+    });
+    message.channel
+      .bulkDelete(fetched)
+      .catch(error =>
+        message.reply(`Couldn't delete messages because of: ${error}`)
+      );
   }
 });
 
