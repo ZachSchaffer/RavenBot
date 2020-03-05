b292385aed219481c8a2a09e6e47e064acba0829
Zach Schaffer <zachary.schaffer12@gmail.com>
2020-03-04 23:43:56 -0500

    more logging

--- index.js
+++ index.js
@@ -63,13 +63,16 @@ bot.on("message", async message => {
 
 async function ban(args, message) {
   if (!message.mentions.members.first()) {
+    console.log(
+      message.author.tag + " tried to ban " + message.mentions.members.first().tag
+    );
     return message.channel.send(
       "Invalid target. This incident will be recorded."
     );
   }
 
   console.log(
-    message.author.tag + " tried to ban " + message.mentions.members.first().tag
+    message.author.tag + " tried to ban " + message.mentions.members.first().tag;
   );
   if (
     !message.member.roles.some(r => ["Administrator", "Admin"].includes(r.name))
