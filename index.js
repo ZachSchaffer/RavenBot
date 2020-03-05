0d1bc9cb484589a10a7f91075bfe1102f5cfd6fe
Zach Schaffer <zachary.schaffer12@gmail.com>
2020-03-04 23:46:32 -0500

    Testing in prod

--- index.js
+++ index.js
@@ -64,9 +64,7 @@ bot.on("message", async message => {
 async function ban(args, message) {
   if (!message.mentions.members.first()) {
     console.log(
-      message.author.tag +
-        " tried to ban " +
-        message.mentions.members.first().tag
+      message.author.tag + " tried to ban " + message.mentions.members.first()
     );
     return message.channel.send(
       "Invalid target. This incident will be recorded."
@@ -74,7 +72,7 @@ async function ban(args, message) {
   }
 
   console.log(
-    message.author.tag + " tried to ban " + message.mentions.members.first().tag
+    message.author.tag + " tried to ban " + message.mentions.members.first()
   );
   if (
     !message.member.roles.some(r => ["Administrator", "Admin"].includes(r.name))
